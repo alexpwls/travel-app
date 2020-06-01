573ea1d74d07de134681972ade55241a7d2e97b0
Alex Pauwels <alexpauwels@MacBook-Pro-van-Alex.local>
2020-06-01 17:57:30 +0200

    Added API endpoints and keys

--- src/server/server.js
+++ src/server/server.js
@@ -10,6 +10,16 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('dist'));
+
+const geonames_account = process.env.GEONAMES_ACCOUNT;
+const geonames_url = process.env.GEONAMES_URL;
+
+const weatherbit_api_key = process.env.WEATHERBIT_API_KEY;
+const weatherbit_current_weather_url = process.env.WEATHERBIT_CURRENT_WEATHER_URL;
+const weatherbit_forecast_16d_url = process.env.WEATHERBIT_FORECAST_16D_URL;
+
+const pixabay_api_key = process.env.PIXABAY_API_KEY;
+const pixabay_url = process.env.PIXABAY_URL;
  
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
@@ -20,3 +30,5 @@ app.listen(8081, function () {
 })
 
 module.exports = app;
+
+console.log(pixabay_url);
